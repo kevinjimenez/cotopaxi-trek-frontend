235a553072bb3cf65003cea41bad16b694bd071e
kevzs <orlando.kj24@gmail.com>
2026-08-03 17:12:31 -0500

    🔧 chore: cargar locale es de dayjs y validar env al iniciar

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import 'dayjs/locale/es';
 import App from './App.vue';
+import './config/env';
 import router from './router';
 
 const pinia = createPinia();
