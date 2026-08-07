0ec728517d97c4145aabd8180ea0facd79d27464
kevzs <orlando.kj24@gmail.com>
2026-08-07 10:50:23 -0500

    ✨ feat: agregar prefix y suffix (texto o ícono) a BaseInput

--- src/shared/components/ui/BaseInput.vue
+++ src/shared/components/ui/BaseInput.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import { Input } from '@/shadcn/ui/input';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+  InputGroupText,
+} from '@/shadcn/ui/input-group';
 import { cn } from '@/shadcn/utils';
+import type { LucideIcon } from '@lucide/vue';
 import type { HTMLAttributes } from 'vue';
 
 interface Props {
   label?: string;
   placeholder?: string;
-  type?: 'email';
+  type?: 'email' | 'number';
+  prefix?: string;
+  prefixIcon?: LucideIcon;
+  suffix?: string;
+  suffixIcon?: LucideIcon;
   class?: HTMLAttributes['class'];
 }
 
@@ -14,8 +24,24 @@ const props = defineProps<Props>();
 </script>
 
 <template>
+  <!-- <div :class="cn('grid w-full items-center gap-y-2', props.class)">
+    <label v-if="label" class="uppercase font-bold text-xs">{{ label }}</label>
+    <Input :type="type" :placeholder="placeholder" v-maska="'#,##0'" />
+  </div> -->
   <div :class="cn('grid w-full items-center gap-y-2', props.class)">
     <label v-if="label" class="uppercase font-bold text-xs">{{ label }}</label>
-    <Input :type="type" :placeholder="placeholder" />
+    <InputGroup>
+      <InputGroupAddon v-if="prefixIcon || prefix">
+        <component :is="prefixIcon" v-if="prefixIcon" class="size-4" />
+        <InputGroupText v-else>{{ prefix }}</InputGroupText>
+      </InputGroupAddon>
+
+      <InputGroupInput :type="type" :placeholder="placeholder" />
+
+      <InputGroupAddon v-if="suffixIcon || suffix" align="inline-end">
+        <component :is="suffixIcon" v-if="suffixIcon" class="size-4" />
+        <InputGroupText v-else>{{ suffix }}</InputGroupText>
+      </InputGroupAddon>
+    </InputGroup>
   </div>
 </template>
