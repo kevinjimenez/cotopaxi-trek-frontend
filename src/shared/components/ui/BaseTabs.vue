e0bfe9692c6294bbca3cd47d2d4d14bf4a21e128
kevzs <orlando.kj24@gmail.com>
2026-08-07 11:24:31 -0500

    ✨ feat: agregar BaseTabs con contenido por slot dinámico
    Cada tab define su propio slot con nombre (item.tab), así cada uso
    del componente pone su contenido sin que BaseTabs sepa nada de él.

---
+++ src/shared/components/ui/BaseTabs.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shadcn/ui/tabs';
+interface Props {
+  tabs: readonly {
+    tab: string;
+    name: string;
+    default: boolean;
+  }[];
+  // default: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <Tabs :default-value="props.tabs.find((i) => i.default)?.tab">
+    <TabsList>
+      <TabsTrigger v-for="(item, index) in props.tabs" :key="index" :value="item.tab">
+        {{ item.name }}
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent v-for="(item, index) in props.tabs" :key="index" :value="item.tab">
+      <slot :name="item.tab" />
+    </TabsContent>
+  </Tabs>
+</template>
