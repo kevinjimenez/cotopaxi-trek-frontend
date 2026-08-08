05900a8f78b2d3c9e1d98ee5895edc54f87b5990
kevzs <orlando.kj24@gmail.com>
2026-08-08 00:14:04 -0500

    ✨ feat: estilo underline en BaseTabs (sin fondo, borde inferior primary)

--- src/shared/components/ui/BaseTabs.vue
+++ src/shared/components/ui/BaseTabs.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shadcn/ui/tabs';
+
+interface Tab {
+  tab: string;
+  name: string;
+  default: boolean;
+}
+
 interface Props {
-  tabs: readonly {
-    tab: string;
-    name: string;
-    default: boolean;
-  }[];
-  // default: string;
+  tabs: readonly Tab[];
 }
 
 const props = defineProps<Props>();
+const defaultTab = props.tabs.find((i) => i.default)?.tab;
 </script>
 
 <template>
-  <Tabs :default-value="props.tabs.find((i) => i.default)?.tab">
-    <TabsList>
-      <TabsTrigger v-for="(item, index) in props.tabs" :key="index" :value="item.tab">
+  <Tabs :default-value="defaultTab">
+    <TabsList class="bg-white">
+      <TabsTrigger
+        v-for="(item, index) in props.tabs"
+        :key="index"
+        :value="item.tab"
+        class="data-[state=active]:bg-transparent data-[state=active]:shadow-none data-[state=active]:border-b-2 data-[state=active]:border-b-primary data-[state=active]:text-black data-[state=inactive]:text-muted-foreground data-[state=active]:rounded-none"
+      >
         {{ item.name }}
       </TabsTrigger>
     </TabsList>
