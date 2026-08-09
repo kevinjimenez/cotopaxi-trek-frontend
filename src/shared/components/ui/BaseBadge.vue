f7a2003a2636ed98161d24cb2124fbaa4acb4814
kevzs <orlando.kj24@gmail.com>
2026-08-09 13:33:02 -0500

    ✨ feat: agregar prefixIcon y suffixIcon a BaseBadge

--- src/shared/components/ui/BaseBadge.vue
+++ src/shared/components/ui/BaseBadge.vue
@@ -2,16 +2,24 @@
 import { Badge } from '@/shadcn/ui/badge';
 import type { ButtonVariants } from '@/shadcn/ui/button';
 import { cn } from '@/shadcn/utils';
+import type { LucideIcon } from '@lucide/vue';
 import type { HTMLAttributes } from 'vue';
 interface Props {
   label: string | number;
+  prefixIcon?: LucideIcon;
+  suffixIcon?: LucideIcon;
   variant?: ButtonVariants['variant'];
   class?: HTMLAttributes['class'];
+  iconClass?: string;
 }
 
 const props = defineProps<Props>();
 </script>
 
 <template>
-  <Badge :class="cn(props.class)">{{ label }}</Badge>
+  <Badge :class="cn('flex flex-row gap-x-1 items-center', props.class)">
+    <component :is="prefixIcon" v-if="prefixIcon" :class="iconClass" />
+    {{ label }}
+    <component :is="suffixIcon" v-if="suffixIcon" :class="iconClass" />
+  </Badge>
 </template>
